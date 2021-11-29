ab0e7478122d0394c15f4504d1e24468d9b01021
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-11-29 22:00:52 +0530

    UC1 Added

---
+++ src/com/BinaryTree/BinaryMain.java
@@ -0,0 +1,15 @@
+package com.BinaryTree;
+
+public class BinaryMain {
+	
+	public static void main(String[] args) {
+		
+		BinaryTree call = new BinaryTree();
+		call.insert(56);
+		call.insert(30);
+		call.insert(70);
+	}
+	
+}
+
+
---
+++ src/com/BinaryTree/Node.java
@@ -0,0 +1,24 @@
+package com.BinaryTree;
+
+public class Node implements Comparable<Node> {
+
+	int data;
+	Node right;
+	Node left;
+	
+	public Node(int data) {
+		super();
+		this.data = data;
+	}
+
+	@Override
+	public int compareTo(Node node) {
+		
+		if(this.data > node.data)
+			return 1;
+		else if (this.data < node.data)
+			return -1;
+		else
+		return 0;
+	}
+}
